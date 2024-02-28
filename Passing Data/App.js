b8efaac20e9aed82c0862b633a2beef8c6ff306b
Shekhar Roy <43708251+Shekhar-roy-5292@users.noreply.github.com>
2024-02-28 12:24:34 +0530

    Passing Data
    1. Passing data Parent to child
    2. Passing data Child to Parent

---
+++ Passing Data/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+import ParentToChild from './Component/Parent/Parent'
+import ChildToParent from './Component/Child/Parent'
+
+
+function App() {
+  return (
+    <div >
+
+
+        <ParentToChild/>
+
+        <br/><br/><br/>
+
+        <ChildToParent/>
+
+
+    </div>
+  );
+}
+
+export default App;
